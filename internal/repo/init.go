0581be95c80998858d3176cbeeda0b309ebeeb78
SANJOY <paulsanjoy2923@gmail.com>
2025-09-08 09:48:10 +0530

    repo/init.go created

---
+++ internal/repo/init.go
@@ -0,0 +1,57 @@
+package repo
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func InitRepo() {
+	DirName := ".gitgo"
+
+	// .gitgo dir creation
+	if err := os.Mkdir(DirName, 0755); err != nil && !os.IsExist(err) {
+		fmt.Println("Error creating .gitgo: ", err)
+		return
+	}
+
+	//.gitgo/objects dir
+	ObjectsPath := filepath.Join(DirName, "objects")
+	if err := os.Mkdir(ObjectsPath, 0755); err != nil && !os.IsExist(err) {
+		fmt.Println("Error creating objects: ", err)
+		return
+	}
+
+	//.gitgo/refs dir
+	RefsPath := filepath.Join(DirName, "refs")
+	if err := os.Mkdir(RefsPath, 0755); err != nil && !os.IsExist(err) {
+		fmt.Println("Error creating refs: ", err)
+		return
+	}
+
+	// .gitgo/refs/heads dir
+	Refs_HeadsPath := filepath.Join(RefsPath, "heads")
+	if err := os.Mkdir(Refs_HeadsPath, 0755); err != nil && !os.IsExist(err) {
+		fmt.Println("Error creating refs: ", err)
+		return
+	}
+
+	// HEAD file
+	HeadFile := filepath.Join(DirName, "HEAD")
+	/*here firstwe are cheching that if the HeadFile exists or not, if not exists then it will trigger next if block. in next if block
+	er are writing the HEAD file which is storing the current branch, for now it doesnot support multiple branch */
+	if _, err := os.Stat(HeadFile); os.IsNotExist(err) {
+		if err := os.WriteFile(HeadFile, []byte("ref: refs/heads/master"), 0644); err != nil {
+			fmt.Println("Error creating HEAD: ", err)
+			return
+		}
+	}
+
+	//index file
+	IndexFile := filepath.Join(DirName, "index")
+	if _, err := os.Stat(IndexFile); os.IsNotExist(err) {
+		if err := os.WriteFile(IndexFile, []byte(""), 0644); err != nil {
+			fmt.Println("Error creating the index: ", err)
+		}
+	}
+}
